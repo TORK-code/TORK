c927faecca0c4791e2e8423ee334bba610f2c960
TORK <brsk2802@gmail.com>
2025-12-30 14:36:46 +0300

    Update BasicBEP20.sol

--- contracts/BasicBEP20.sol
+++ contracts/BasicBEP20.sol
@@ -1,1 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
 
+contract BEP20 {
+    string public name = "Your_token_name";
+    string public symbol = "Your_token_symbol";
+    uint8 public decimals = 18;
+
+    uint256 public totalSupply;
+    address public owner;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    constructor(uint256 _totalSupply) {
+        owner = msg.sender;
+        totalSupply = _totalSupply * 10 ** decimals;
+        balanceOf[msg.sender] = totalSupply;
+
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function transfer(address to, uint256 value) external returns (bool) {
+        require(balanceOf[msg.sender] >= value, "Balance low");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 value) external returns (bool) {
+        allowance[msg.sender][spender] = value;
+
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool) {
+        require(balanceOf[from] >= value, "Balance low");
+        require(allowance[from][msg.sender] >= value, "Allowance low");
+
+        allowance[from][msg.sender] -= value;
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    function burn(uint256 value) external {
+        require(balanceOf[msg.sender] >= value, "Balance low");
+
+        balanceOf[msg.sender] -= value;
+        totalSupply -= value;
+
+        emit Transfer(msg.sender, address(0), value);
+    }
+
+    function renounceOwnership() external onlyOwner {
+        emit OwnershipTransferred(owner, address(0));
+        owner = address(0);
+    }
+}
